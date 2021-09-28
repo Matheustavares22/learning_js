0a47525ca435899d7af3ad20245c1a1d7753c9fb
Matheus Tavares <matheus.tav.nun@gmail.com>
2021-09-27 23:00:32 -0300

    some basic operations

---
+++ scripts/class1/myFirstProgram.js
@@ -0,0 +1,12 @@
+console.log('hello world: working with variables');
+
+const age = 21;
+
+console.log('matheus age is ' + age);
+console.log('matheus age is ' + (age + 2));
+console.log('matheus age is ' + (age / 2));
+console.log('matheus age is ' + (age - 2));
+console.log('matheus age is ' + (age * 2));
+
+const calculetedAge = age + 2;
+console.log(calculetedAge);
